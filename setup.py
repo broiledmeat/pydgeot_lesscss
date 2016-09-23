c6b2deed0f1b88f05464b39410cdcf6122185597
Derrick Staples <broiledmeat@gmail.com>
2016-09-23 09:44:27 -0400

    Update setup and add manifest

--- setup.py
+++ setup.py
@@ -1,14 +1,23 @@
 #!/usr/bin/env python3
 from distutils.core import setup
 
+
 setup(
     name='pydgeot_lesscss',
-    version='0.1',
-    packages=['pydgeot.plugins.lesscss'],
-    requires=['pydgeot', 'lesscpy'],
+    description='LessCSS support for Pydgeot.',
     url='https://github.com/broiledmeat/pydgeot_lesscss',
     license='Apache License, Version 2.0',
     author='Derrick Staples',
     author_email='broiledmeat@gmail.com',
-    description='LessCSS support for Pydgeot.'
+    version='0.2',
+    packages=['pydgeot.plugins.lesscss'],
+    requires=['pydgeot', 'lesscpy'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Text Processing :: Markup'
+    ]
 )
